e6da1550781e1b7539ee3302955b9c622d5ea880
LopesZ3R4 <jl.moreno7@hotmail.com>
2023-09-18 22:55:14 -0400

    adding jwt authentication

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 
 [ApiController]
 [Route("[controller]")]
@@ -14,13 +15,15 @@ public class AuthController : ControllerBase
 {
     private readonly AuthenticationService _authService;
     private readonly UserRepository _userRepository;
+    private readonly IConfiguration _configuration;
 
-    public AuthController(AuthenticationService authService, UserRepository userRepository) // Modify this line
+    public AuthController(AuthenticationService authService, UserRepository userRepository, IConfiguration configuration)
     {
         _authService = authService;
         _userRepository = userRepository;
+        _configuration = configuration;
     }
-
+    [HttpPost("login")]
     public IActionResult Login([FromBody] LoginRequest request)
     {
         var isValid = _authService.ValidateCredentials(request.Username, request.Password);
@@ -30,7 +33,6 @@ public class AuthController : ControllerBase
             return Unauthorized();
         }
 
-        // Create a claim
         var claims = new[]
         {
             new Claim(JwtRegisteredClaimNames.Sub, request.Username),
@@ -40,24 +42,21 @@ public class AuthController : ControllerBase
         // Generate token
         var token = new JwtSecurityToken
         (
-            issuer: "your_issuer",
-            audience: "your_audience",
+            issuer: _configuration["Jwt:Issuer"],
             claims: claims,
             expires: DateTime.UtcNow.AddDays(1),
             notBefore: DateTime.UtcNow,
-            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_secret_key")), SecurityAlgorithms.HmacSha256)
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])), SecurityAlgorithms.HmacSha256)
         );
 
         var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
 
-        // Retrieve user from database
         var user = _userRepository.Get(request.Username);
 
         // Assign token to user and save changes
         user.Token = tokenString;
-        _userRepository.Update(user); // You'll need to implement this method in your UserRepository
+        _userRepository.Update(user);
 
-        // Send token to client
         return Ok(new { Token = tokenString });
     }
 
