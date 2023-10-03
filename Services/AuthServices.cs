408756e6511fb2887c2d13048fc73b6b64c22e0a
LopesZ3R4 <jl.moreno7@hotmail.com>
2023-10-02 23:12:00 -0400

    get user from token method

--- Services/AuthServices.cs
+++ Services/AuthServices.cs
@@ -1,16 +1,21 @@
 // .\Services\AuthServices
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using Data;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Services {
     public class AuthenticationService
     {
         private readonly UserRepository _userRepository;
+        private readonly IConfiguration _configuration;
 
-    public AuthenticationService(UserRepository userRepository)
+    public AuthenticationService(UserRepository userRepository, IConfiguration configuration)
     {
         _userRepository = userRepository;
+        _configuration = configuration;
     }
 
     public bool ValidateCredentials(string Username, string password)
@@ -36,6 +41,23 @@ namespace Services {
             var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
             return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
         }
+    }
+    public string GetUsernameFromToken(string token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var validations = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])),
+            ValidateIssuer = true,
+            ValidIssuer = _configuration["Jwt:Issuer"],
+            ValidateAudience = false
+        };
+
+        var claims = handler.ValidateToken(token, validations, out _);
+        var usernameClaim = claims.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+        return usernameClaim?.Value;
     }
     }
 }
